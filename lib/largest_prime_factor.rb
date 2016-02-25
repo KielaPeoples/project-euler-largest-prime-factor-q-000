b70c339350136655442bbf3db7b531863daadf8d
KielaPeoples <kielapeoples@gmail.com>
2016-02-25 02:13:44 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+  prime_arr = []
+  counter = 2
+  while input > 1
+    while input % counter == 0
+      prime_arr.push(counter)
+      input = input / counter
+    end
+    counter += 1
+  end
+  return prime_arr.max
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(input)
+    @input = input
+  end
+
+  def number
+    prime_arr = []
+    counter = 2
+    while @input > 1
+      while @input % counter == 0
+        prime_arr.push(counter)
+        @input = @input / counter
+      end
+      counter += 1
+    end
+    return prime_arr.max
+  end
+end
